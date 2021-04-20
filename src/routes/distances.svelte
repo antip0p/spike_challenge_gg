eea223adbef62fe8c3d020d4e4b05602dedcbdd6
Gustavo González <gsgonzalez@gmail.com>
2021-04-19 21:03:08 -0400

    Componente para lista de queries rendereando con SSR.

---
+++ src/routes/distances.svelte
@@ -0,0 +1,21 @@
+<script context="module">
+  export async function preload(page, session) {
+    const res = await this.fetch('/api/distances');
+    const distanceRows = await res.json();
+		return { distanceRows };
+	}
+</script>
+
+<script>
+  import { DataTable } from "carbon-components-svelte";
+  export let distanceRows;
+</script>
+
+<DataTable
+  headers={[
+    { key: 'address0', value: 'Punto A' },
+    { key: 'address1', value: 'Punto B' },
+    { key: 'distance', value: 'Distancia' }
+  ]}
+  rows={distanceRows}
+/>
